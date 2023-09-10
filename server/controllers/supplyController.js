8757d3230a3c66b43071898bf4193d9e8f9a9341
joel <joelcel@student.chalmers.se>
2023-09-10 22:26:11 +0200

    Add supply entity
    This commit adds the basic implmentation for the supply entity. It has a model-, controller- and router class and it has basic automated tests that are run with Postman.

---
+++ server/controllers/supplyController.js
@@ -0,0 +1,82 @@
+const SupplyModel = require('../models/supplyModel');
+
+class SupplyController {
+
+    async createSupply(req, res) {
+    
+        try {
+          const itemname = req.body.itemname;  
+          const instock = req.body.instock;
+          const userid = req.body.userid;
+          const newSupply = new SupplyModel({
+            "itemname": itemname,
+            "instock": instock,
+            "userid": userid,
+          })
+          await newSupply.save();
+          res.status(201).json(newSupply);
+    
+        } catch (err) {
+          res.status(500).json({err: "Internal Server Error"});
+        }
+      }
+  
+    async getAllSupplies(req, res) {
+    try {
+      const supply = await SupplyModel.find({});
+      res.json(supply);
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  }
+
+  async getSupplyById(req, res) {
+    const id = req.params.id;
+
+    try {
+        const supply = await SupplyModel.findById(id);
+        if (!supply) {
+            return res.status(404).send('Supply not found');
+        }
+        res.json(supply);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+  }
+
+  async deleteSupplyById(req, res) {
+    const id = req.params.id;
+
+    try {
+        const supply = await SupplyModel.findByIdAndDelete(id);
+        if (!supply) {
+            return res.status(404).send('Supply not found');
+        }
+        res.status(200).send(`Supply with ID ${id} was deleted.`);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+}
+
+async updateSupplyById(req, res){
+  const id = req.params.id;
+  const updateData = req.body;
+
+  try {
+    const supply = await SupplyModel.findById(id);
+    if (!supply) {
+        return res.status(404).send('Supply not found');
+    }
+    const updatedSupply = await SupplyModel.findByIdAndUpdate(id, updateData, { new: true });  // { new: true }
+    res.status(200).json(updatedSupply);
+} catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+}
+
+};
+
+module.exports = SupplyController;
