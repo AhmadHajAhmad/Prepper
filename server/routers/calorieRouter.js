e78b36068b17a2cbabe8b6285922c0a90f898cbb
joel <joelcel@student.chalmers.se>
2023-09-19 13:32:35 +0200

    Added Authentication
    This commit adds the authentication middleware to the router class.

--- server/routers/calorieRouter.js
+++ server/routers/calorieRouter.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const CalorieController = require("../controllers/calorieController");
+const authentication = require("../middleware/authenticateToken");
 
 const router = express.Router({ mergeParams: true });
 const controller = new CalorieController();
 
-router.get("/days", controller.getDaysOfFood);
-router.get("/stored", controller.getTotalStored);
-router.get("/needs", controller.getTotalIntake);
+router.get("/days",authentication, controller.getDaysOfFood);
+router.get("/stored",authentication, controller.getTotalStored);
+router.get("/needs",authentication, controller.getTotalIntake);
 module.exports = router;
