224fcba407166e3bb39aaddc08849e1892a3c0ee
haja <haja@student.chalmers.se>
2023-10-09 21:50:17 +0200

    Implement the error handling of the Food page
    The user cannot insert invalid fields to create a food

--- client/src/views/Food.vue
+++ client/src/views/Food.vue
@@ -1,76 +1,174 @@
 <template>
   <div class="navbar-container">
-    <NavbarInternal/>
+    <NavbarInternal />
     <div class="main-container">
-      <div class="login-container d-flex justify-content-center align-items-start vh-100">
-      <div class="col-12 col-md-10 col-lg-8 p-5">
-      <h1>Current Food</h1>
-      <div class="list-group" v-if="foodList && foodList.length">
-        <div v-for="item in foodList" :key="item._id" class="list-group-item d-flex justify-content-between align-items-center">
-            <!-- If edit mode is on, display input fields for updating -->
-            <template v-if="editMode[item._id]">
-                <input v-model="item.foodname" @blur="updateFood(item)" class="form-control mb-2"/>
-                <input v-model="item.weight" @blur="updateFood(item)" class="form-control mb-2"/>
-                <input v-model="item.calories" @blur="updateFood(item)" class="form-control mb-2"/>
-            </template>
-            <!-- If edit mode is off, display text with ability to click into edit mode -->
-            <template v-else>
+      <div
+        class="login-container d-flex justify-content-center align-items-start vh-100"
+      >
+        <div class="col-12 col-md-10 col-lg-8 p-5">
+          <h1>Current Food</h1>
+          <div class="list-group" v-if="foodList && foodList.length">
+            <div
+              v-for="item in foodList"
+              :key="item._id"
+              class="list-group-item d-flex justify-content-between align-items-center"
+            >
+              <!-- If edit mode is on, display input fields for updating -->
+              <template v-if="editMode[item._id]">
+                <input
+                  v-model="item.foodname"
+                  @blur="updateFood(item)"
+                  class="form-control mb-2"
+                />
+                <input
+                  v-model="item.weight"
+                  @blur="updateFood(item)"
+                  class="form-control mb-2"
+                />
+                <input
+                  v-model="item.calories"
+                  @blur="updateFood(item)"
+                  class="form-control mb-2"
+                />
+              </template>
+              <!-- If edit mode is off, display text with ability to click into edit mode -->
+              <template v-else>
                 <div>
-                    <strong>Name: </strong>
-                    <span class="me-3" @click="toggleEdit(item)">{{ item.foodname }}</span>
-                    <strong>Weight: </strong>
-                    <span class="me-3" @click="toggleEdit(item)">{{ item.weight }}<strong>g</strong></span>
-                    <strong>Calories: </strong>
-                    <span @click="toggleEdit(item)">{{ item.calories }}<strong>kcal</strong></span>
+                  <strong>Name: </strong>
+                  <span class="me-3" @click="toggleEdit(item)">{{
+                    item.foodname
+                  }}</span>
+                  <strong>Weight: </strong>
+                  <span class="me-3" @click="toggleEdit(item)"
+                    >{{ item.weight }}<strong>g</strong></span
+                  >
+                  <strong>Calories: </strong>
+                  <span @click="toggleEdit(item)"
+                    >{{ item.calories }}<strong>kcal</strong></span
+                  >
                 </div>
                 <!-- Delete Button for each item -->
-                <button @click="deleteFood(item)" class="btn btn-dark">Delete</button>
-            </template>
-        </div>
-      </div>
-      <p v-if="error" class="text-danger mt-2">{{ error }}</p>
+                <button @click="deleteFood(item)" class="btn btn-dark">
+                  Delete
+                </button>
+              </template>
+            </div>
+          </div>
+          <p v-if="error" class="text-danger mt-2">{{ error }}</p>
 
-      <!-- Button to open the modal -->
-      <button class="btn btn-light" data-bs-toggle="modal" data-bs-target="#addFoodModal">Add Food</button>
+          <!-- Button to open the modal -->
+          <button @click="createFood" class="btn btn-light">Add Food</button>
 
-      <!-- Bootstrap Modal for Adding Food -->
-      <div class="modal fade" id="addFoodModal" tabindex="-1" aria-labelledby="addFoodModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="addFoodModalLabel">Add New Food</h5>
-              <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+          <!-- Bootstrap Modal for Adding Food -->
+          <div
+            v-if="showForm"
+            class="modal"
+            id="addFoodModal"
+            tabindex="1"
+            aria-labelledby="addFoodModalLabel"
+            aria-hidden="true"
+            data-bs-backdrop="static"
+          >
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="addFoodModalLabel">
+                    Add New Food
+                  </h5>
+                </div>
+                <form
+                  action=""
+                  @submit.prevent="addFood"
+                  class="was-validated form-floating"
+                >
+                  <div class="modal-body form-floating">
+                    <div class="mb-3 form floating">
+                      <input
+                        v-model="newFood.foodname"
+                        type="text"
+                        placeholder="Name"
+                        class="form-control"
+                        required
+                        pattern="^[A-Za-z]+(\s[A-Za-z]+)*$"
+                      />
+                      <div class="valid-feedback">looks good!</div>
+                      <div class="invalid-feedback">
+                        Only Characters accepted!
+                      </div>
+                    </div>
+                    <div class="mb-3">
+                      <input
+                        v-model="newFood.weight"
+                        type="number"
+                        placeholder="Weight"
+                        class="form-control"
+                        required
+                        pattern="^[1-9]\d*$"
+                      />
+                      <div class="valid-feedback">looks good!</div>
+                      <div class="invalid-feedback">
+                        Only Positive Numbers accepted!
+                      </div>
+                    </div>
+                    <div class="mb-3">
+                      <input
+                        v-model="newFood.calories"
+                        type="number"
+                        placeholder="Calories"
+                        class="form-control"
+                        required
+                        pattern="^[1-9]\d*$"
+                      />
+                      <div class="valid-feedback">looks good!</div>
+                      <div class="invalid-feedback">
+                        Only Positive Numbers accepted!
+                      </div>
+                    </div>
+                  </div>
+                  <div class="modal-footer">
+                    <button class="btn btn-primary">Save</button>
+                    <button @click="cancel" class="btn btn-secondary">
+                      Cancel
+                    </button>
+                  </div>
+                </form>
+              </div>
             </div>
-            <div class="modal-body">
-              <input v-model="newFood.foodname" type="text" placeholder="Name" class="form-control mb-2">
-              <input v-model="newFood.weight" type="number" placeholder="Weight" class="form-control mb-2">
-              <input v-model="newFood.calories" type="number" placeholder="Calories" class="form-control mb-2">
+          </div>
+
+          <div>
+            <!-- Progress Bar -->
+            <div class="progress">
+              <div
+                class="progress-bar"
+                role="progressbar"
+                :style="{ width: `${waterQuantity * 4}%` }"
+                :aria-valuenow="waterQuantity"
+                aria-valuemin="0"
+                aria-valuemax="25"
+              >
+                {{ waterQuantity }} Liters
+              </div>
             </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-dark" data-bs-dismiss="modal">Close</button>
-                <button type="button" @click="addFood" class="btn btn-light" data-bs-dismiss="modal">Add Food</button>
-            </div>
+            <!-- Control Buttons -->
+            <button class="btn btn-light mt-3" @click="increaseWater">
+              Increase
+            </button>
+            <button class="btn btn-dark mt-3 ms-2" @click="decreaseWater">
+              Decrease
+            </button>
           </div>
         </div>
       </div>
-      <div>
-        <!-- Progress Bar -->
-        <div class="progress">
-            <div class="progress-bar" role="progressbar" :style="{width: `${waterQuantity * 4}%`}" :aria-valuenow="waterQuantity" aria-valuemin="0" aria-valuemax="25">{{ waterQuantity }} Liters</div>
-        </div>
-        <!-- Control Buttons -->
-        <button class="btn btn-light mt-3" @click="increaseWater">Increase</button>
-        <button class="btn btn-dark mt-3 ms-2" @click="decreaseWater">Decrease</button>
     </div>
-</div>
-</div>
-</div>
-<NavbarInternalBottom/>
-</div>
+    <NavbarInternalBottom />
+  </div>
 </template>
 
 <script>
-import { ref, reactive, onMounted } from 'vue'
+import { ref, reactive, onMounted, watch, nextTick } from 'vue'
+import * as bootstrap from 'bootstrap'
+import { Modal } from 'bootstrap'
 import axios from 'axios'
 import NavbarInternal from '../components/NavbarInternal.vue'
 import NavbarInternalBottom from '../components/NavbarInternalBottom.vue'
@@ -90,10 +188,30 @@ export default {
       weight: null,
       calories: null
     })
+    const showForm = ref(false)
     const waterQuantity = ref(0)
     const editMode = reactive({})
     const userid = ref(sessionStorage.getItem('userId'))
     const token = ref(sessionStorage.getItem('token'))
+
+    watch(showForm, (newVal) => {
+      if (newVal) {
+        // If showForm becomes true
+        nextTick(() => {
+          const modalElement = document.querySelector('.modal')
+          if (modalElement) {
+            const modalInstance = new Modal(modalElement)
+            modalInstance.show()
+          } else {
+            console.error('Modal element not found')
+          }
+        })
+      }
+    })
+    const createFood = () => {
+      newFood.value = resetForm()
+      showForm.value = true
+    }
 
     const getFood = async () => {
       try {
@@ -146,6 +264,7 @@ export default {
             }
           }
         )
+        cancel()
         getFood()
       } catch (error) {
         console.error('Error adding food:', error)
@@ -154,11 +273,14 @@ export default {
 
     const deleteFood = async (foodItem) => {
       try {
-        await axios.delete(`http://localhost:3000/v1/profiles/${userid.value}/food/${foodItem._id}`, {
-          headers: {
-            usertoken: token.value
+        await axios.delete(
+          `http://localhost:3000/v1/profiles/${userid.value}/food/${foodItem._id}`,
+          {
+            headers: {
+              usertoken: token.value
+            }
           }
-        })
+        )
         getFood()
       } catch (error) {
         console.error('Error deleting food:', error)
@@ -186,15 +308,11 @@ export default {
     const updateWater = async (waterData) => {
       const link = `http://localhost:3000${waterLinks.value.update.href}`
       try {
-        await axios.put(
-          link,
-          waterData,
-          {
-            headers: {
-              usertoken: token.value
-            }
+        await axios.put(link, waterData, {
+          headers: {
+            usertoken: token.value
           }
-        )
+        })
       } catch (error) {
         console.error('Error updating water:', error)
       }
@@ -220,11 +338,27 @@ export default {
 
     onMounted(() => {
       getFood()
-      foodList.value.forEach(item => {
+      foodList.value.forEach((item) => {
         editMode[item._id] = false
       })
       getWater()
     })
+    const cancel = () => {
+      const modalElement = document.querySelector('.modal') // or use refs.modal if you have ref="modal" on the root modal div
+      if (modalElement) {
+        const modalInstance = bootstrap.Modal.getInstance(modalElement)
+        modalInstance.hide()
+      }
+      showForm.value = false
+      newFood.value = resetForm()
+    }
+    const resetForm = () => {
+      return {
+        foodname: '',
+        weight: null,
+        calories: null
+      }
+    }
 
     return {
       foodList,
@@ -244,13 +378,16 @@ export default {
       updateWater,
       toggleEdit,
       increaseWater,
-      decreaseWater
+      decreaseWater,
+      showForm,
+      cancel,
+      createFood
     }
   }
 }
 </script>
- <style scoped>
- @keyframes waterEffect {
+<style scoped>
+@keyframes waterEffect {
   0% {
     background-position-x: 0;
   }
@@ -260,7 +397,16 @@ export default {
 }
 
 .progress-bar {
-  background-image: linear-gradient(45deg, #3498db 25%, transparent 25%, transparent 50%, #3498db 50%, #3498db 75%, transparent 75%, transparent);
+  background-image: linear-gradient(
+    45deg,
+    #3498db 25%,
+    transparent 25%,
+    transparent 50%,
+    #3498db 50%,
+    #3498db 75%,
+    transparent 75%,
+    transparent
+  );
   background-size: 50px 50px;
   animation: waterEffect 1s linear infinite;
 }
