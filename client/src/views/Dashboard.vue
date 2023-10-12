5d258d5d7ea5484dbddb80d6fafe07a15300bc73
haja <haja@student.chalmers.se>
2023-10-12 20:01:46 +0200

    Implement the status of food and water in the dashboard page

--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="navbar-container">
-    <NavbarInternal/>
+    <NavbarInternal />
 
     <!-- Bootstrap container for a responsive layout -->
     <div class="main-container col-lg-8 col-md-10 col-sm-10 col-12 mx-auto">
       <div class="row">
-
         <!-- First Column -->
         <div class="col-lg-4 col-md-4 col-sm-12 p-3 centered-content">
           <p>Food:</p>
           <ve-progress
-          :progress="calories * 10"
-          :legend="calories"
-          thickness="7%"
-          animation="duration 1500"
-          color="#982560">
+            :progress="calories * 10"
+            :legend="calories"
+            thickness="7%"
+            animation="duration 1500"
+            color="#982560"
+          >
             <template #legend-caption>
               <p>Days Stored</p>
             </template>
@@ -25,10 +25,11 @@
         <div class="col-lg-4 col-md-4 col-sm-12 p-3 centered-content">
           <p>Water:</p>
           <ve-progress
-          :progress="water * 10"
-          :legend="water"
-          thickness="7%"
-          animation="duration 1500">
+            :progress="water * 10"
+            :legend="water"
+            thickness="7%"
+            animation="duration 1500"
+          >
             <template #legend-caption>
               <p>Days Stored</p>
             </template>
@@ -37,25 +38,30 @@
 
         <!-- Third Column -->
         <div class="col-lg-4 col-md-4 col-sm-12 p-3 centered-content">
-          <p>Placeholder:</p>
-          <p>
-            Lorem ipsum dolor sit amet. Et alias molestiae aut impedit aspernatur et quas accusamus qui laborum quas ut nobis dolorem ea voluptates quidem. Qui voluptatem velit sit animi ratione eos omnis illo qui sint iste a veritatis omnis eos galisum nemo?
-          </p>
-        </div>
+          <p>Food Status:</p>
+          <div class="status-message">
+            {{ foodStatusMessage }}
+          </div>
 
+          <p>Water Status:</p>
+          <div class="status-message">
+            {{ waterStatusMessage }}
+          </div>
+        </div>
       </div>
     </div>
 
-    <NavbarInternalBottom/>
+    <NavbarInternalBottom />
   </div>
 </template>
 
 <script>
-import { ref, onBeforeMount } from 'vue'
+import { ref, onBeforeMount, computed } from 'vue'
 import axios from 'axios'
 import { useRouter } from 'vue-router'
 import NavbarInternal from '../components/NavbarInternal.vue'
 import NavbarInternalBottom from '../components/NavbarInternalBottom.vue'
+import { getFoodStatusMessage, getWaterStatusMessage } from '../statusMessages'
 
 export default {
   components: {
@@ -84,9 +90,12 @@ export default {
 
     async function getCalories() {
       try {
-        const response = await axios.get(`http://localhost:3000/v1/profiles/${userId.value}/calories/days`, {
-          headers: { usertoken: token.value }
-        })
+        const response = await axios.get(
+          `http://localhost:3000/v1/profiles/${userId.value}/calories/days`,
+          {
+            headers: { usertoken: token.value }
+          }
+        )
         calories.value = response.data.toFixed(2)
       } catch (error) {
         console.log(error)
@@ -95,20 +104,35 @@ export default {
 
     async function getWater() {
       try {
-        const response = await axios.get(`http://localhost:3000/v1/profiles/${userId.value}/calories/water`, {
-          headers: { usertoken: token.value }
-        })
+        const response = await axios.get(
+          `http://localhost:3000/v1/profiles/${userId.value}/calories/water`,
+          {
+            headers: { usertoken: token.value }
+          }
+        )
         water.value = response.data.toFixed(2)
       } catch (error) {
         console.log(error)
       }
     }
+    const foodStatusMessage = computed(() => {
+      if (calories.value === null) return 'You have zero food' // Optional loading message
+      return getFoodStatusMessage(calories.value)
+    })
+
+    const waterStatusMessage = computed(() => {
+      if (water.value === null) return 'You have Zero water' // Optional loading message
+      return getWaterStatusMessage(water.value)
+    })
 
     async function getPeople() {
       try {
-        const response = await axios.get(`http://localhost:3000/v1/profiles/${userId.value}/people`, {
-          headers: { usertoken: token.value }
-        })
+        const response = await axios.get(
+          `http://localhost:3000/v1/profiles/${userId.value}/people`,
+          {
+            headers: { usertoken: token.value }
+          }
+        )
         people.value = response.data.length
       } catch (error) {
         console.log(error)
@@ -122,7 +146,9 @@ export default {
       people,
       getCalories,
       getWater,
-      getPeople
+      getPeople,
+      foodStatusMessage,
+      waterStatusMessage
     }
   }
 }
